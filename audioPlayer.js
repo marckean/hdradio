ebc0d752fa74a262f28498cf8402de06b8ee310a
Marc Kean <marc@marckean.com>
2023-09-04 08:56:12 +1000

    .

---
+++ audioPlayer.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const audio = document.getElementById('myAudio');
+    const hls = new Hls();
+
+    document.getElementById('audioMenu').addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        const newAudioSrc = e.target.getAttribute('data-audio-src');
+        const audioType = e.target.getAttribute('data-audio-type');
+        
+        if (newAudioSrc) {
+            if (audioType === 'm3u8') {
+                if (Hls.isSupported()) {
+                    hls.loadSource(newAudioSrc);
+                    hls.attachMedia(audio);
+                    hls.on(Hls.Events.MANIFEST_PARSED, function() {
+                        audio.play();
+                    });
+                } else if (audio.canPlayType('application/vnd.apple.mpegurl')) {
+                    audio.src = newAudioSrc;
+                    audio.addEventListener('loadedmetadata', function() {
+                        audio.play();
+                    });
+                }
+            } else {
+                audio.src = newAudioSrc;
+                audio.type = `audio/${audioType}`;
+                audio.play();
+            }
+        }
+    });
+});
