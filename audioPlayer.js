5d576152550c0f7b5177c2ece5a9904ddf129705
Marc Kean <marc@marckean.com>
2023-09-04 10:09:26 +1000

    .

--- audioPlayer.js
+++ audioPlayer.js
@@ -31,47 +31,29 @@
 //     });
 // });
 
-// Add an event listener for the entire audioMenu div
-document.addEventListener('DOMContentLoaded', (event) => {
-
-    // Fetch the CSV and parse it
-    fetch('./stations/StreamDatabase.csv')
-        .then(response => response.text())
-        .then(data => {
-            const rows = data.split('\n');
-            for (let i = 1; i < rows.length; i++) { // Skip header row
-                const row = rows[i].split(',');
-                const stationName = row[0];
-                const state = row[1];
-                const url = row[2];
-                const logo = row[3];
-                const audioType = row[4];
-
-                // Create a new div for each grid item
-                const gridItem = document.createElement('div');
-                gridItem.className = 'grid-item';
-
-                // Create an image element and set its properties
-                const imgElement = document.createElement('img');
-                imgElement.src = logo;
-                imgElement.alt = stationName;
-                imgElement.title = stationName;
-                imgElement.style.cursor = 'pointer';
-                imgElement.style.width = '100%';  // Responsive image
-
-                // Add an event listener for the image element
-                imgElement.addEventListener('click', function() {
-                    const audioElement = document.getElementById('myAudio');
-                    audioElement.src = url;
-                    audioElement.type = `audio/${audioType}`;
-                    audioElement.play();
-                });
-
-                // Append image to grid item and grid item to audio menu
-                gridItem.appendChild(imgElement);
-                document.getElementById('audioMenu').appendChild(gridItem);
-            }
-        });
-
-});
+document.addEventListener("DOMContentLoaded", function() {
+    const audioElement = document.getElementById("myAudio");
+  
+    const audioMenu = document.getElementById("audioMenu");
+    audioMenu.addEventListener("click", function(event) {
+      const target = event.target;
+      if (target.tagName === "A") {
+        event.preventDefault();
+        
+        const audioSrc = target.getAttribute("data-audio-src");
+        const audioType = target.getAttribute("data-audio-type");
+        
+        if (audioType === "mp3") {
+          audioElement.src = audioSrc;
+        } else if (audioType === "m3u8" && Hls.isSupported()) {
+          var hls = new Hls();
+          hls.loadSource(audioSrc);
+          hls.attachMedia(audioElement);
+        }
+        
+        audioElement.play();
+      }
+    });
+  });
+  
 
