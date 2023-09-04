bd33e9455c5109992b38d39b8b3dd43502c94d88
Marc Kean <marc@marckean.com>
2023-09-04 10:35:04 +1000

    .

--- audioPlayer.js
+++ audioPlayer.js
@@ -31,30 +31,3 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     const audioElement = document.getElementById("myAudio");
-  
-//     const audioMenu = document.getElementById("audioMenu");
-//     audioMenu.addEventListener("click", function(event) {
-//       const target = event.target;
-//       if (target.tagName === "A") {
-//         event.preventDefault();
-        
-//         const audioSrc = target.getAttribute("data-audio-src");
-//         const audioType = target.getAttribute("data-audio-type");
-        
-//         if (audioType === "mp3") {
-//           audioElement.src = audioSrc;
-//         } else if (audioType === "m3u8" && Hls.isSupported()) {
-//           var hls = new Hls();
-//           hls.loadSource(audioSrc);
-//           hls.attachMedia(audioElement);
-//         }
-        
-//         audioElement.play();
-//       }
-//     });
-//   });
-  
-
